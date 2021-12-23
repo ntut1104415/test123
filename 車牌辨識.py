294868772acf59eeb30d90c77cdf2641e1913d2a
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-23 20:50:43 +0800

    Update 車牌辨識.py

--- 車牌辨識.py
+++ 車牌辨識.py
@@ -8,15 +8,27 @@ import os
 import shutil
 from datetime import datetime
 import numpy as np
-import pandas as pd
-import tarfile
-from io import BytesIO
-from base64 import b64encode
-from github import Github
-from IPython.display import Markdown, display
+   
+Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
+        if Image is not None:
+            col1, col2 = st.beta_columns(2)
+            Image = Image.read()
+            Image = tf.image.decode_image(Image, channels=3).numpy()                  
+            Image = adjust_gamma(Image, gamma=gamma)
+            with col1:
+                st.image(Image)
+            input_image = loadtest(Image,cropornot=Autocrop)
+            prediction = comic_model(input_image, training=True)
+            prediction = tf.squeeze(prediction,0)
+            prediction = prediction* 0.5 + 0.5
+            prediction = tf.image.resize(prediction, 
+                           [outputsize, outputsize],
+                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            prediction=  prediction.numpy()
+            with col2:
+                st.image(prediction)
 
-img = input_img()
-cv.imshow('img',img)
+
 #img = cv2.imread("car2.jpg")
 #cv2.imshow("input",img)
 
